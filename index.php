fe94f7003802f1e3e91ae3952a1aea2e1e92b1cf
Marco-ram-p <162069517+Marco-ram-p@users.noreply.github.com>
2025-04-18 16:19:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "✅ Configuración guardada.";
 }
 ?>
-
-<form method="post">
+<html>
+   <form method="post">
     <label>API 1: <input type="text" name="api1"></label><br>
     <label>API 2: <input type="text" name="api2"></label><br>
     <label>Valor Mínimo: <input type="number" name="min" step="any"></label><br>
     <label>Valor Máximo: <input type="number" name="max" step="any"></label><br><br>
     <button type="submit">Guardar</button>
 </form>
+ 
+</html>
